b5771d0acf65d0427cd7876d1046562d2714b95a
Oren <orengolan@gmail.com>
2015-05-09 16:04:41 -0700

    process.exit tests

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var http = require('http');
 var server = http.createServer(function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.write('io.js in a minimal docker image\n');
+  process.exit(1);
   res.end();
 });
 
