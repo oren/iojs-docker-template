1422f5d6c23024827bb2d802d92478b7b1783cda
Oren <orengolan@gmail.com>
2015-05-09 18:07:38 -0700

    remove exit(1)

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var http = require('http');
 var server = http.createServer(function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.write('io.js in a minimal docker image\n');
-  process.exit(1);
   res.end();
 });
 
